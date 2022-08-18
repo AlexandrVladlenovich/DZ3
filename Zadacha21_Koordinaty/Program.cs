eb9febad2f58422eceb5b662fb9d310bbede57af
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-18 13:09:57 +0500

    add Zadacha21_Koordinaty2

--- Zadacha21_Koordinaty/Program.cs
+++ Zadacha21_Koordinaty/Program.cs
@@ -1,5 +1,4 @@
 ﻿Console.Clear();
-char selection;
 Double x1, x2, y1, y2, z1, z2, distance;
 Console.Write("Введите координаты x1: ");
     x1 = Convert.ToDouble(Console.ReadLine());
@@ -14,6 +13,6 @@ Console.Write("Введите координаты z1: ");
 Console.Write("Введите координаты z2: ");
     z2 = Convert.ToDouble(Console.ReadLine());
 
-    distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-        Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
-        Console.ReadKey();
+distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
+
