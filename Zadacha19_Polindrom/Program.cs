7b7157ffe0becfddfbe296b2eb9073e9dfefa177
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-18 12:11:18 +0500

    add Zadacha19_Polindrom

---
+++ Zadacha19_Polindrom/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int i, remainder = 0, newnum = 0;
+    Console.WriteLine("Введите число: ");
+    int uinput = Convert.ToInt32((Console.ReadLine()));
+        for (i = uinput; i > 0; i = (i / 10))
+        {
+            remainder = i % 10;
+            newnum = (newnum*10) + remainder;
+        }
+        if (newnum == uinput)
+        {
+            Console.WriteLine(" - число является полиндромом");
+        }
+        else
+        {
+            Console.WriteLine(" - число не является полиндромом");
+        }
+Console.ReadKey();
