1aa6573d2b0ee26aab4556649ade18f0f1c8649a
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-18 14:31:34 +0500

    add Zadacha23_KubChisel

---
+++ Zadacha23_KubChisel/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine());
+    for (int i = 0; i <= n; i++)
+        Console.WriteLine("{0}^3 = {1}", i, i*i*i);
+Console.ReadKey(true);
